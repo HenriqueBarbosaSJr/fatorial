6a4321525d83156f846762aea0b8252ce06b3476
HenriqueBarbosaSJr <henrique@hbveterinaria.com.br>
2026-09-08 21:30:41 -0300

    Ajuste do comando de calculo do fatorial

--- fatorial.c
+++ fatorial.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-float num, resultFat=1, i;
+float num, 
+resultFat=1, i;
 int main(){
     printf("Digite um número para ser calculado = ");
     scanf("%f",&num);
 
     for(i=1; i<=num; i++){
-        resultFat= resultFat * i; 
+        resultFat *= i; 
     }
     printf("Resultado = %.0f ",resultFat);
     return 0;
